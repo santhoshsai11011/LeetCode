f15f553e1f36aea7d771c83884148638d1b0a28e
santhoshsai11011 <santhoshsai11011@gmail.com>
2025-10-09 23:32:56 +0530

    Time: 0 ms (100%), Space: 45.7 MB (22.29%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int maxSum = n*(n+1)/2;
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum = sum+nums[i];
+        }
+        return maxSum-sum;
+    }
+}
