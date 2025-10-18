d2a1f22d91e7e2d8ef5781f9b61444cefd5d9f11
santhoshsai11011 <santhoshsai11011@gmail.com>
2025-10-18 16:13:30 +0530

    Time: 682 ms (14.87%), Space: 0B (100%) - LeetHub

--- 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
@@ -2,13 +2,13 @@
 
 SELECT e.employee_id AS employee_id
 FROM Employees e
-LEFT JOIN Salaries s
+LEFT JOIN Salaries s 
 ON e.employee_id = s.employee_id
-WHERE s.salary IS NULL 
+WHERE s.salary IS NULL
 UNION
 SELECT s.employee_id AS employee_id
 FROM Salaries s
 LEFT JOIN Employees e
 ON s.employee_id = e.employee_id
 WHERE e.name IS NULL
-ORDER BY employee_id ASC
+ORDER BY employee_id ASC;
