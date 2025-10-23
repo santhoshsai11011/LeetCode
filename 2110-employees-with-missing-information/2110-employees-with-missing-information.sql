b35563a0f953a099fbcca37dd2f3483c7019615e
santhoshsai11011 <santhoshsai11011@gmail.com>
2025-10-23 07:29:09 +0530

    Time: 527 ms (63.1%), Space: 0B (100%) - LeetHub

--- 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
@@ -1,14 +1,16 @@
 # Write your MySQL query statement below
 
-SELECT e.employee_id AS employee_id
-FROM Employees e
-LEFT JOIN Salaries s 
-ON e.employee_id = s.employee_id
-WHERE s.salary IS NULL
-UNION
-SELECT s.employee_id AS employee_id
-FROM Salaries s
+WITH cte AS
+(SELECT employee_id FROM Employees
+ UNION
+ SELECT employee_id FROM Salaries
+)
+
+SELECT c.employee_id 
+FROM cte c
 LEFT JOIN Employees e
-ON s.employee_id = e.employee_id
-WHERE e.name IS NULL
-ORDER BY employee_id ASC;
+ON e.employee_id = c.employee_id
+LEFT JOIN Salaries s
+ON s.employee_id = c.employee_id
+WHERE e.name IS NULL OR s.salary IS NULL
+ORDER BY c.employee_id ASC;
