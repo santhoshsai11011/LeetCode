96bda8c3346e953ff4cf11ab40ded944a73893d9
santhoshsai11011 <santhoshsai11011@gmail.com>
2025-10-23 07:34:16 +0530

    Time: 517 ms (69.93%), Space: 0B (100%) - LeetHub

--- 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
@@ -1,16 +1,29 @@
 # Write your MySQL query statement below
 
-WITH cte AS
-(SELECT employee_id FROM Employees
- UNION
- SELECT employee_id FROM Salaries
-)
+-- WITH cte AS
+-- (SELECT employee_id FROM Employees
+--  UNION
+--  SELECT employee_id FROM Salaries
+-- )
 
-SELECT c.employee_id 
-FROM cte c
-LEFT JOIN Employees e
-ON e.employee_id = c.employee_id
+-- SELECT c.employee_id 
+-- FROM cte c
+-- LEFT JOIN Employees e
+-- ON e.employee_id = c.employee_id
+-- LEFT JOIN Salaries s
+-- ON s.employee_id = c.employee_id
+-- WHERE e.name IS NULL OR s.salary IS NULL
+-- ORDER BY c.employee_id ASC;
+
+SELECT e.employee_id
+FROM Employees e 
 LEFT JOIN Salaries s
-ON s.employee_id = c.employee_id
-WHERE e.name IS NULL OR s.salary IS NULL
-ORDER BY c.employee_id ASC;
+ON e.employee_id = s.employee_id
+WHERE s.salary IS NULL
+UNION
+SELECT s.employee_id
+FROM Salaries s
+LEFT JOIN Employees e
+ON s.employee_id = e.employee_id
+WHERE e.name IS NULL
+ORDER BY employee_id ASC;
