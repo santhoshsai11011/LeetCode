247a07bfff3966df53fa4f6e4fa8e7081ee90270
santhoshsai11011 <santhoshsai11011@gmail.com>
2025-11-21 08:47:05 +0530

    Time: 49 ms (13.21%), Space: 42.4 MB (7.95%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -8,6 +8,7 @@ class Solution {
     public int fib(int n) {
         int [] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return f(n,dp);
+        int ans = f(n,dp);
+        return ans;
     }
 }
