df0ae65863b61d8edd23486531ca8e00d2ad3324
santhoshsai11011 <santhoshsai11011@gmail.com>
2025-11-29 23:18:40 +0530

    Time: 7 ms (43.34%), Space: 42.2 MB (10.66%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int f(int n){
+        if(n<=1) return n;
+        return f(n-1) + f(n-2);
+    }
+    public int fib(int n) {
+        int ans = f(n);
+        return ans;
+    }
+}
