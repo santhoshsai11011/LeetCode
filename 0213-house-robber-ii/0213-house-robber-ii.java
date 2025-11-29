dbf73f99815311f24f628c326023115e6b828330
santhoshsai11011 <santhoshsai11011@gmail.com>
2025-11-29 17:49:18 +0530

    Time: 0 ms (100%), Space: 42.5 MB (43.17%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,13 +1,4 @@
 class Solution {
-    static int f(int n,int [] nums,int [] dp){
-        if(n==0) return nums[0];
-        if(n<0) return 0;
-        if(dp[n] != -1) return dp[n];
-        int pick = nums[n] + f(n-2,nums,dp);
-        int notPick = f(n-1,nums,dp);
-        dp[n] =  Math.max(pick,notPick);
-        return dp[n];
-    }
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==0) return 0;
@@ -19,12 +10,26 @@ class Solution {
             nums1[i] = nums[i];
         }
         for(int i=1;i<n;i++){
-            nums2[i-1] = nums[i];  
+            nums2[i-1] = nums[i];
         }
-         Arrays.fill(dp,-1);
-        int ans1 = f(n-2,nums1,dp);
-         Arrays.fill(dp,-1);
-        int ans2 = f(n-2,nums2,dp);
+        Arrays.fill(dp,-1);
+        dp[0] = nums1[0];
+        for(int i=1;i<n-1;i++){
+            int pick = nums1[i];
+            if(i>1) pick += dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = Math.max(pick,notPick);
+        }
+        int ans1 = dp[n-2];
+        Arrays.fill(dp,-1);
+        dp[0] = nums2[0];
+        for(int i=1;i<n-1;i++){
+            int pick = nums2[i];
+            if(i>1) pick += dp[i-2];
+            int notPick = dp[i-1];
+            dp[i] = Math.max(pick,notPick);
+        }
+        int ans2 = dp[n-2];
         int ans = Math.max(ans1,ans2);
         return ans;
     }
