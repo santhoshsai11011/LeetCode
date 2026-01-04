9dcdad7f787fa28ef32e4a23942b1c94671043a9
santhoshsai11011 <santhoshsai11011@gmail.com>
2026-01-04 12:31:34 +0530

    Time: 27 ms (48.65%), Space: 47.8 MB (11.19%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,23 +1,14 @@
 class Solution {
-    static int f(String s1,String s2,int i,int j,int [][] dp){
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1.charAt(i) == s2.charAt(j)){
-            dp[i][j] = 1+f(s1,s2,i-1,j-1,dp);
-            return dp[i][j];
-        }
-        dp[i][j] = Math.max(f(s1,s2,i-1,j,dp),f(s1,s2,i,j-1,dp));
-        return dp[i][j];
-    }
     public int minInsertions(String s) {
         int n = s.length();
         String t = new StringBuilder(s).reverse().toString();
-        int [][] dp = new int[n][n];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        int [][] dp = new int[n+1][n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
         }
-        int lcs = f(s,t,n-1,n-1,dp);
-        int ans = n-lcs;
-        return ans;
+        return n-dp[n][n];
     }
 }
