9bf78d1271d093a25e472fe58bd16fa983556d7e
santhoshsai11011 <santhoshsai11011@gmail.com>
2026-01-05 20:15:51 +0530

    Time: 3 ms (96.53%), Space: 47 MB (82.13%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,31 @@
+class Solution {
+    static int f(String s1,String s2,int i,int j,int [][] dp){
+        if(j<0) return i+1;
+        if(i<0) return j+1;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1.charAt(i) == s2.charAt(j)){
+            dp[i][j] = f(s1,s2,i-1,j-1,dp);;
+            return dp[i][j];
+        }
+        else{
+            int insert = 1+f(s1,s2,i,j-1,dp);
+            int delete = 1+f(s1,s2,i-1,j,dp);
+            int replace = 1+f(s1,s2,i-1,j-1,dp);
+            dp[i][j] = Math.min(insert,Math.min(delete,replace));
+            return dp[i][j];
+        }
+    }
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        if(word1.equals(word2)) return 0;
+        if(n==0) return m;
+        if(m==0) return n;
+        int [][] dp = new int[n][m];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        int ans = f(word1,word2,n-1,m-1,dp);
+        return dp[n-1][m-1];
+    }
+}
