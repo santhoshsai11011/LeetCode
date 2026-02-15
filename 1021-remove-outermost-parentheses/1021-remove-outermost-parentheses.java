db172bd668066c9f64817d4dbfaf6494fd230728
santhoshsai11011 <santhoshsai11011@gmail.com>
2026-02-16 02:41:20 +0530

    Time: 12 ms (6.88%), Space: 47 MB (8.35%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> st = new Stack<>();
+        int counter = 0;
+        String ans = "";
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch == ')'){
+                st.pop();
+                if(st.size() >0) ans += ')' +"";
+            }
+            else{
+                if(st.size() >0) ans += '('+"";
+                st.push(ch);
+            }
+        }
+        return ans;
+    }
+}
