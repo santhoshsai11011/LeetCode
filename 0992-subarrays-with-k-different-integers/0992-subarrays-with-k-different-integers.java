0737aabfdf3c2243fdb0a0d73fc16cc63fb343f0
santhoshsai11011 <santhoshsai11011@gmail.com>
2026-02-24 11:48:32 +0530

    Time: 53 ms (18.82%), Space: 50 MB (66.82%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,27 +1,28 @@
 class Solution {
     static int f(int [] nums,int k){
         Map<Integer,Integer> mp = new HashMap<>();
-        int n = nums.length;
         int l = 0;
         int r = 0;
+        int n = nums.length;
         int count = 0;
         while(r<n){
-            if(mp.containsKey(nums[r])) mp.put(nums[r],mp.get(nums[r])+1);
-            else mp.put(nums[r],1);
-            while(mp.size()>k){
-                mp.put(nums[l],mp.get(nums[l])-1);
-                if(mp.get(nums[l]) == 0) mp.remove(nums[l]);
+            int x = nums[r];
+            if(mp.containsKey(x)) mp.put(x,mp.get(x)+1);
+            else mp.put(x,1);
+            while(l<r && mp.size() > k){
+                int y = nums[l];
+                mp.put(y,mp.get(y)-1);
+                if(mp.get(y) == 0) mp.remove(y);
                 l++;
             }
-            if(mp.size()<=k){
-                count += r-l+1;
+            if(mp.size() <= k){
+                count += (r-l+1);
             }
             r++;
         }
         return count;
     }
     public int subarraysWithKDistinct(int[] nums, int k) {
-        int ans = f(nums,k)-f(nums,k-1);
-        return ans;
+        return f(nums,k)-f(nums,k-1);
     }
 }
