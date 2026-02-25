7b83c6f19eb4537fc45ecbad2b9f5b7f8c99242b
santhoshsai11011 <santhoshsai11011@gmail.com>
2026-02-26 00:30:55 +0530

    Time: 0 ms (100%), Space: 42.9 MB (82.89%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -4,10 +4,10 @@ class Solution {
         int count = 0;
         for(int i=0;i<n-1;i++){
             if(nums[i] > nums[i+1]) count++;
-            if(count>1) return false;
+            if(count > 1) return false;
         }
         if(nums[n-1] > nums[0]) count++;
-        if(count <=1) return true;
-        return false;
+        if(count > 1) return false;
+        return true;
     }
 }
