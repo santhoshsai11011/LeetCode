20c3c313ada6039c0f6b269687c273024b197cc1
santhoshsai11011 <santhoshsai11011@gmail.com>
2026-03-07 12:04:52 +0530

    Time: 15 ms (10.79%), Space: 44.1 MB (74.7%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void bfs1(TreeNode root,Map<TreeNode,TreeNode> parents){
+        if(root == null) return;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode node = q.remove();
+            if(node.left != null){
+                q.add(node.left);
+                parents.put(node.left,node);
+            }
+            if(node.right != null){
+                q.offer(node.right);
+                parents.put(node.right,node);
+            }
+        }
+    }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        List<Integer> ans = new ArrayList<>();
+        Map<TreeNode,TreeNode> parents = new HashMap<>();
+        bfs1(root,parents);
+        Map<TreeNode,Boolean> visited = new HashMap<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(target);
+        visited.put(target,true);
+        int currentLevel = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            if(currentLevel == k) break;
+            currentLevel++;
+            for(int i=0;i<size;i++){
+                TreeNode node = q.remove();
+                if(node.left != null && visited.get(node.left) == null){
+                    q.offer(node.left);
+                    visited.put(node.left,true);
+                }
+                if(node.right != null && visited.get(node.right) == null){
+                    q.offer(node.right);
+                    visited.put(node.right,true);
+                }
+                if(parents.get(node) != null && visited.get(parents.get(node)) == null){
+                    q.offer(parents.get(node));
+                    visited.put(parents.get(node),true);
+                }
+            }
+        }
+
+        while(!q.isEmpty()){
+            ans.add(q.remove().val);
+        }
+        return ans;
+    }
+}
