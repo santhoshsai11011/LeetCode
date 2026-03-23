ef6ba83e4adf53aaa1deea86d14a4d01d17c0b2a
santhoshsai11011 <santhoshsai11011@gmail.com>
2026-03-23 18:06:21 +0530

    Time: 31 ms (52.5%), Space: 146.8 MB (61.08%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -5,7 +5,7 @@ class Solution {
         int [] ans = new int[n-k+1];
         int x = 0;
         for(int i=0;i<n;i++){
-            if(!dq.isEmpty() && dq.peekFirst() <= i-k) dq.pollFirst();
+            if(!dq.isEmpty() && dq.peekFirst() == i-k) dq.pollFirst();
             while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast();
             dq.offerLast(i);
             if(i >= k-1) ans[x++] = nums[dq.peekFirst()];
